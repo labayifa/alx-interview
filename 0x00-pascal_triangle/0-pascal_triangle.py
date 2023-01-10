6b1bafc6cee64ca26e6110ee089fb090bad89d77
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 15:29:34 +0100

    pascal ok

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+pascal_triangle(n):  returns a list of lists of integers representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    res = []
+    if int(n) <= 0:
+        return []
+    else:
+        for i in range(0, n):
+            box = []
+            for j in range(0, i + 1):
+                box.append(triangle(i, j))
+            res.append(box)
+    return res
+
+
+"""
+    triangle(i, j): get the value at position i,j
+"""
+
+
+def triangle(i, j):
+    if i == j or i <= 1 or j == 0:
+        return 1
+    elif j > i:
+        return 0
+    else:
+        return triangle(i - 1, j - 1) + triangle(i - 1, j)
