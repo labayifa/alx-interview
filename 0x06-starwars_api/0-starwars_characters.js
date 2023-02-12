ffa6ac45421d3133a7627930f28909c74f79e109
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-02-12 11:28:59 +0100

    stars wars fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,15 +4,16 @@ const request = require('request');
 const idFilm = process.argv[2];
 
 request(`https://swapi-api.alx-tools.com/api/films/${idFilm}`, async function (error, response, body) {
-  const film = JSON.parse(body);
   if (error) {
     throw error;
   }
 
+  const film = JSON.parse(body);
+
   if (film !== undefined && Array.isArray(film.characters)) {
     const characters = film.characters;
 
-    for (character of characters) {
+    for (const character of characters) {
       await new Promise((resolve, reject) => {
         request(character, function (error, response, body) {
           if (error) {
